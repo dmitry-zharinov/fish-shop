df1d722614331f35bd503b4a654ff7e28904b960
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-12-17 09:07:21 +0000

    Format code

--- bot.py
+++ bot.py
@@ -4,37 +4,46 @@ import os
 import redis
 from dotenv import load_dotenv
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
-from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
-                          MessageHandler, Updater)
+from telegram.ext import (
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+    Updater,
+)
 
-from moltin import (add_product_to_cart, add_product_id_to_cart, download_product_image,
-                    get_access_token, get_cart_items, get_product,
-                    get_product_price, get_product_stock, get_products, get_cart, remove_product_from_cart)
+from moltin import (
+    add_product_id_to_cart,
+    download_product_image,
+    get_access_token,
+    get_cart_items,
+    get_product,
+    get_products,
+    get_cart,
+    remove_product_from_cart,
+)
 
 _database = None
-logger = logging.getLogger('tg-bot')
+logger = logging.getLogger("tg-bot")
 
 
 def start(update, context):
-    db = context.bot_data['db']
-    moltin_token = db.get('moltin_token').decode("utf-8")
+    db = context.bot_data["db"]
+    moltin_token = db.get("moltin_token").decode("utf-8")
     products = get_products(moltin_token)
     keyboard = [
-        [InlineKeyboardButton(product['name'],
-                              callback_data=product['id'])]
+        [InlineKeyboardButton(product["name"], callback_data=product["id"])]
         for product in products
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Выберите товар:', reply_markup=reply_markup)
-    return 'HANDLE_MENU'
+    update.message.reply_text("Выберите товар:", reply_markup=reply_markup)
+    return "HANDLE_MENU"
 
 
 def handle_menu(update, context):
-    logging.info(f'handle_menu')
-
-    db = context.bot_data['db']
-    moltin_token = db.get('moltin_token').decode('utf-8')
-    #price_book_id = db.get('price_book_id').decode("utf-8")
+    db = context.bot_data["db"]
+    moltin_token = db.get("moltin_token").decode("utf-8")
+    # price_book_id = db.get('price_book_id').decode("utf-8")
     context.bot.delete_message(
         chat_id=update.effective_chat.id,
         message_id=update.callback_query.message.message_id,
@@ -42,49 +51,47 @@ def handle_menu(update, context):
     callback = update.callback_query.data
     product_id = callback
     product = get_product(product_id, moltin_token)
-    image_id = product['relationships']['main_image']['data']['id']
+    image_id = product["relationships"]["main_image"]["data"]["id"]
 
     product_image = download_product_image(image_id, moltin_token)
-    product_description = product['description']
-    #product_price = get_product_price(moltin_token, price_book_id, product['attributes']['sku'])
-    #product_stock = get_product_stock(moltin_token, product_id)
-    #price = product_price['attributes']['currencies']['USD']['amount']
-    #stock = product_stock['available']
+    product_description = product["description"]
 
-    reply_markup = InlineKeyboardMarkup([
+    reply_markup = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton('ДОбавить в корзину', callback_data=f'{product_id}~1'),
-        ],
-        [InlineKeyboardButton('Корзина', callback_data='cart')],
-        [InlineKeyboardButton('Назад', callback_data='go_back')],
-    ])
+            [
+                InlineKeyboardButton(
+                    "ДОбавить в корзину", callback_data=f"{product_id}~1"
+                ),
+            ],
+            [InlineKeyboardButton("Корзина", callback_data="cart")],
+            [InlineKeyboardButton("Назад", callback_data="go_back")],
+        ]
+    )
 
-    with open(product_image, 'rb') as photo:
+    with open(product_image, "rb") as photo:
         context.bot.send_photo(
             chat_id=update.effective_chat.id,
             photo=photo,
             caption=product_description,
             reply_markup=reply_markup,
         )
-    return 'HANDLE_DESCRIPTION'
+    return "HANDLE_DESCRIPTION"
 
 
 def handle_description(update, context):
-    logging.info(f'handle_description')
-
-    db = context.bot_data['db']
-    token = db.get('moltin_token').decode('utf-8')
+    db = context.bot_data["db"]
+    token = db.get("moltin_token").decode("utf-8")
     callback = update.callback_query.data
 
-    if callback == 'cart':
+    if callback == "cart":
         show_cart_items(update, context, token)
         return "HANDLE_CART"
-    elif callback != 'go_back':
+    elif callback != "go_back":
         add_item_to_cart(update, context, token)
         return "HANDLE_DESCRIPTION"
     else:
         show_items_menu(update, context, token)
-        return 'HANDLE_MENU'
+        return "HANDLE_MENU"
 
 
 def show_items_menu(update, context, token):
@@ -94,77 +101,75 @@ def show_items_menu(update, context, token):
     )
     products = get_products(moltin_token)
     keyboard = [
-        [InlineKeyboardButton(
-            product['name'],
-            callback_data=product['id'])
-        ]
+        [InlineKeyboardButton(product["name"], callback_data=product["id"])]
         for product in products
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text='Выберите товар:',
-        reply_markup=reply_markup
+        text="Выберите товар:",
+        reply_markup=reply_markup,
     )
 
 
 def add_item_to_cart(update, context, token):
     callback = update.callback_query.data
-    product_id, quantity = callback.split('~')
+    product_id, quantity = callback.split("~")
     product = get_product(product_id, token)
     cart_id = update.effective_chat.id
-    add_product_id_to_cart(
-        token,
-        cart_id,
-        product['sku'],
-        int(quantity)
-    )
+    add_product_id_to_cart(token, cart_id, product["sku"], int(quantity))
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=f"Товар {product['name']} добавлен в корзину!"
-        )
+        text=f"Товар {product['name']} добавлен в корзину!",
+    )
 
 
 def show_cart_items(update, context, token):
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="<b>Ваша корзина:</b>",
-            parse_mode=ParseMode.HTML
-        )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="<b>Ваша корзина:</b>",
+        parse_mode=ParseMode.HTML,
+    )
 
-        cart_id = update.effective_chat.id
-        cart = get_cart(token, cart_id)
-        cart_items = get_cart_items(token, cart_id)
-        text = [
-            f"""{num+1}. {product['name']}
+    cart_id = update.effective_chat.id
+    cart = get_cart(token, cart_id)
+    cart_items = get_cart_items(token, cart_id)
+    text = [
+        f"""{num+1}. {product['name']}
             {product['meta']['display_price']['with_tax']['unit']['formatted']} за шт.
             {product['quantity']} шт. в корзине на сумму {product['meta']['display_price']['with_tax']['value']['formatted']}\n\n"""
-            for num, product in enumerate(cart_items)
-        ]
-        text.append(f"<b>Итого: {cart['meta']['display_price']['with_tax']['formatted']}</b>")
+        for num, product in enumerate(cart_items)
+    ]
+    text.append(
+        f"<b>Итого: {cart['meta']['display_price']['with_tax']['formatted']}</b>"
+    )
 
-        keyboard = [
-            [InlineKeyboardButton(f"Убрать из корзины {product['name']}", callback_data=product['id'])]
-            for product in cart_items
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                f"Убрать из корзины {product['name']}", callback_data=product["id"]
+            )
         ]
-        keyboard.append([InlineKeyboardButton("В меню", callback_data='go_back')])
+        for product in cart_items
+    ]
+    keyboard.append([InlineKeyboardButton("В меню", callback_data="go_back")])
 
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="".join(text),
-            parse_mode=ParseMode.HTML,
-            reply_markup=InlineKeyboardMarkup(keyboard),
-        )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="".join(text),
+        parse_mode=ParseMode.HTML,
+        reply_markup=InlineKeyboardMarkup(keyboard),
+    )
 
 
 def handle_cart(update, context):
-    db = context.bot_data['db']
-    token = db.get('moltin_token').decode('utf-8')
+    db = context.bot_data["db"]
+    token = db.get("moltin_token").decode("utf-8")
     callback = update.callback_query.data
 
-    if callback == 'go_back':
+    if callback == "go_back":
         show_items_menu(update, context, token)
-        return 'HANDLE_MENU'
+        return "HANDLE_MENU"
     else:
         context.bot.delete_message(
             chat_id=update.effective_chat.id,
@@ -173,15 +178,12 @@ def handle_cart(update, context):
         cart_id = update.effective_chat.id
         remove_product_from_cart(token, cart_id, callback)
         context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"Товар удалён из корзины!"
+            chat_id=update.effective_chat.id, text="Товар удалён из корзины!"
         )
-        return 'HANDLE_CART'
+        return "HANDLE_CART"
 
 
 def handle_users_reply(update, context):
-    logging.info(f'handle_users_reply')
-
     db = get_database_connection()
     if update.message:
         user_reply = update.message.text
@@ -191,18 +193,18 @@ def handle_users_reply(update, context):
         chat_id = update.callback_query.message.chat_id
     else:
         return
-    if user_reply == '/start':
-        user_state = 'START'
-    elif user_reply == 'go_back':
-        user_state = 'HANDLE_DESCRIPTION'
+    if user_reply == "/start":
+        user_state = "START"
+    elif user_reply == "go_back":
+        user_state = "HANDLE_DESCRIPTION"
     else:
-        user_state = db.get(chat_id).decode('utf-8')
+        user_state = db.get(chat_id).decode("utf-8")
 
     states_functions = {
-        'START': start,
-        'HANDLE_MENU': handle_menu,
-        'HANDLE_DESCRIPTION': handle_description,
-        'HANDLE_CART': handle_cart,
+        "START": start,
+        "HANDLE_MENU": handle_menu,
+        "HANDLE_DESCRIPTION": handle_description,
+        "HANDLE_CART": handle_cart,
     }
     state_handler = states_functions[user_state]
     try:
@@ -215,37 +217,33 @@ def handle_users_reply(update, context):
 def get_database_connection():
     global _database
     if _database is None:
-        _database = redis.Redis(host=database_host,
-                                port=database_port,
-                                password=database_password)
+        _database = redis.Redis(
+            host=database_host, port=database_port, password=database_password
+        )
     return _database
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
     logging.basicConfig(level=logging.INFO)
 
-    client_id = os.getenv('CLIENT_ID')
-    client_secret = os.getenv('CLIENT_SECRET')
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
     moltin_token = get_access_token(client_id, client_secret)
-    logging.info(f'token: {moltin_token}')
+    logging.info(f"token: {moltin_token}")
 
-    database_password = os.getenv('DATABASE_PASSWORD')
-    database_host = os.getenv('DATABASE_HOST')
-    database_port = os.getenv('DATABASE_PORT')
-    db = redis.Redis(
-        host=database_host,
-        port=database_port,
-        password=database_password
-    )
-    db.set('moltin_token', moltin_token)
-    #price_book_id = os.getenv('PRICE_BOOK_ID')
-    #db.set('price_book_id', price_book_id)
-    
-    updater = Updater(os.getenv('TELEGRAM_TOKEN'))
+    database_password = os.getenv("DATABASE_PASSWORD")
+    database_host = os.getenv("DATABASE_HOST")
+    database_port = os.getenv("DATABASE_PORT")
+    db = redis.Redis(host=database_host, port=database_port, password=database_password)
+    db.set("moltin_token", moltin_token)
+    # price_book_id = os.getenv('PRICE_BOOK_ID')
+    # db.set('price_book_id', price_book_id)
+
+    updater = Updater(os.getenv("TELEGRAM_TOKEN"))
     dispatcher = updater.dispatcher
-    dispatcher.bot_data['db'] = db
+    dispatcher.bot_data["db"] = db
     dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
     dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
-    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    dispatcher.add_handler(CommandHandler("start", handle_users_reply))
     updater.start_polling()
--- moltin.py
+++ moltin.py
@@ -6,147 +6,146 @@ from dotenv import load_dotenv
 
 
 def get_access_token(client_id, client_secret):
-    endpoint = 'https://api.moltin.com/oauth/access_token'
+    endpoint = "https://api.moltin.com/oauth/access_token"
     data = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': 'client_credentials',
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials",
     }
     response = requests.post(endpoint, data=data)
     response.raise_for_status()
-    return response.json()['access_token']
+    return response.json()["access_token"]
 
 
 def get_products(token):
-    endpoint = 'https://api.moltin.com/v2/products'
+    endpoint = "https://api.moltin.com/v2/products"
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
     }
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def get_product(product_id, token):
-    endpoint = f'https://api.moltin.com/v2/products/{product_id}'
+    endpoint = f"https://api.moltin.com/v2/products/{product_id}"
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
     }
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def get_product_image(product_id, token):
-    endpoint = f'https://api.moltin.com/v2/products/{product_id}/relationships/main-image'
+    endpoint = (
+        f"https://api.moltin.com/v2/products/{product_id}/relationships/main-image"
+    )
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
     }
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def download_product_image(image_id, token):
-    endpoint = f'https://api.moltin.com/v2/files/{image_id}'
+    endpoint = f"https://api.moltin.com/v2/files/{image_id}"
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
     }
     response = requests.get(endpoint, headers=headers)
 
     response.raise_for_status()
-    decoded_response = response.json()['data']
+    decoded_response = response.json()["data"]
 
-    image_url = decoded_response['link']['href']
-    image_name = decoded_response['file_name']
-    image_path = os.path.join('tmp', image_name)
+    image_url = decoded_response["link"]["href"]
+    image_name = decoded_response["file_name"]
+    image_path = os.path.join("tmp", image_name)
 
     response = requests.get(image_url)
     response.raise_for_status()
-    with open(image_path, 'wb') as file:
+    with open(image_path, "wb") as file:
         file.write(response.content)
     return image_path
 
 
 def get_file_by_id(file_id, token):
-    endpoint = f'https://api.moltin.com/v2/files/{file_id}'
+    endpoint = f"https://api.moltin.com/v2/files/{file_id}"
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
     }
     response = requests.post(endpoint, headers=headers)
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def create_cart(token, cart_name):
-    endpoint = 'https://api.moltin.com/v2/carts'
+    endpoint = "https://api.moltin.com/v2/carts"
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
     }
     data = {
-        'data': {
-            'name': cart_name,
+        "data": {
+            "name": cart_name,
         }
     }
     response = requests.post(endpoint, headers=headers, json=data)
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def get_cart(token, cart_id):
     response = requests.get(
-        f'https://api.moltin.com/v2/carts/{cart_id}',
-        headers={'Authorization': f'Bearer {token}'}
+        f"https://api.moltin.com/v2/carts/{cart_id}",
+        headers={"Authorization": f"Bearer {token}"},
     )
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def get_cart_items(token, cart_id):
     response = requests.get(
-        f'https://api.moltin.com/v2/carts/{cart_id}/items',
-        headers={'Authorization': f'Bearer {token}'}
+        f"https://api.moltin.com/v2/carts/{cart_id}/items",
+        headers={"Authorization": f"Bearer {token}"},
     )
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def add_product_id_to_cart(token, cart_id, product_sku, quantity):
-    endpoint = f'https://api.moltin.com/v2/carts/{cart_id}/items'
+    endpoint = f"https://api.moltin.com/v2/carts/{cart_id}/items"
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
     }
     product_item = {
-        'data': {
-            'type': 'cart_item',
-            'sku': product_sku,
-            'quantity': quantity,
+        "data": {
+            "type": "cart_item",
+            "sku": product_sku,
+            "quantity": quantity,
         }
     }
-    response = requests.post(
-        endpoint,
-        headers=headers,
-        json=product_item)
+    response = requests.post(endpoint, headers=headers, json=product_item)
     response.raise_for_status()
     return response.json()
 
 
 def add_product_to_cart(token, cart_id, product_sku, quantity, name):
-    logging.info(f'add_product_to_cart')
-    endpoint = f'https://api.moltin.com/v2/carts/{cart_id}/items'
+    logging.info("add_product_to_cart")
+    endpoint = f"https://api.moltin.com/v2/carts/{cart_id}/items"
     headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
     }
 
-    #product_item = {
+    # product_item = {
     #    'data': {
     #        'type': 'custom_item',
     #        'name': product['attributes']['name'],
@@ -157,34 +156,28 @@ def add_product_to_cart(token, cart_id, product_sku, quantity, name):
     #            "amount": 500
     #        }
     #    }
-    #}
+    # }
 
     custom_item = {
-      "data": {
-        "type": "custom_item",
-        "name": name,
-        "sku": product_sku,
-        #"description": "My first custom item!",
-        "quantity": quantity,
-        "price": {
-          "amount": 10000
+        "data": {
+            "type": "custom_item",
+            "name": name,
+            "sku": product_sku,
+            # "description": "My first custom item!",
+            "quantity": quantity,
+            "price": {"amount": 10000},
         }
-      }
     }
-    response = requests.post(
-        endpoint,
-        headers=headers,
-        json=custom_item)
+    response = requests.post(endpoint, headers=headers, json=custom_item)
     response.raise_for_status()
     return response.json()
 
 
-
 def remove_product_from_cart(token, cart_id, product_id):
     response = requests.delete(
-        f'https://api.moltin.com/v2/carts/{cart_id}/items/{product_id}',
+        f"https://api.moltin.com/v2/carts/{cart_id}/items/{product_id}",
         headers={
-            'Authorization': f'Bearer {token}',
+            "Authorization": f"Bearer {token}",
         },
     )
     response.raise_for_status()
@@ -193,57 +186,60 @@ def remove_product_from_cart(token, cart_id, product_id):
 
 def get_price_book_prices(token, price_book_id):
     response = requests.get(
-        f'https://api.moltin.com/pcm/pricebooks/{price_book_id}/prices/',
-        headers={'Authorization': f'Bearer {token}'}
+        f"https://api.moltin.com/pcm/pricebooks/{price_book_id}/prices/",
+        headers={"Authorization": f"Bearer {token}"},
     )
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def get_product_price(token, price_book_id, product_sku):
-    prices = get_price_book_prices(token, price_book_id,)
+    prices = get_price_book_prices(
+        token,
+        price_book_id,
+    )
     price_id = None
     for price in prices:
-        if price['sku'] == product_sku:
-            price_id = price['id']
+        if price["sku"] == product_sku:
+            price_id = price["id"]
 
     if price_id is None:
         return
 
     response = requests.get(
-        f'https://api.moltin.com/pcm/pricebooks/{price_book_id}/prices/{price_id}',
-        headers={'Authorization': f'Bearer {token}'}
+        f"https://api.moltin.com/pcm/pricebooks/{price_book_id}/prices/{price_id}",
+        headers={"Authorization": f"Bearer {token}"},
     )
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def get_product_stock(token, product_id):
     response = requests.get(
-        f'https://api.moltin.com/v2/inventories/{product_id}',
-        headers={'Authorization': f'Bearer {token}'}
+        f"https://api.moltin.com/v2/inventories/{product_id}",
+        headers={"Authorization": f"Bearer {token}"},
     )
     response.raise_for_status()
-    return response.json()['data']
+    return response.json()["data"]
 
 
 def main():
-    client_id = os.getenv('CLIENT_ID'),
-    client_secret = os.getenv('CLIENT_SECRET'),
+    client_id = (os.getenv("CLIENT_ID"),)
+    client_secret = (os.getenv("CLIENT_SECRET"),)
     # Получить токен
     token = get_access_token(client_id, client_secret)
     products = get_products(token)
-    #product_1 = products[0]
+    # product_1 = products[0]
 
     # Создать корзину
-    #cart = create_cart(token, 'test_cart')
+    # cart = create_cart(token, 'test_cart')
     # Добавить продукт в корзину
-    #add_product_to_cart(token, cart['id'], product_1)
-    #refreshed_cart = get_cart(token, cart['id'])
-    #print(json.dumps(refreshed_cart, sort_keys=True, indent=4))
+    # add_product_to_cart(token, cart['id'], product_1)
+    # refreshed_cart = get_cart(token, cart['id'])
+    # print(json.dumps(refreshed_cart, sort_keys=True, indent=4))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
     logging.basicConfig(level=logging.INFO)
 
